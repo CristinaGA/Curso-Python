0763a20b001ecf499ed4add42a23c178a531cd13
Cristina Garrido <cristinagarridoamador@gmail.com>
2021-04-10 19:45:29 +0100

    Delete 10. Errors and Exceptions Handling directory

--- 10. Errors and Exceptions Handling/cap.py
+++
@@ -1,7 +0,0 @@
-def cap_text(text):
-	'''
-	Input a string
-	Output the capitalized string
-	'''
-	#return text.capitalize() #this just capitalize the first letter of a string
-	return text.title() #this capitalize the first letter of each word of a string
